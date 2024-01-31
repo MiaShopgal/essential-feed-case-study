ffffd97680ff2ea4e650278fa1b626a3d86e7c2c
₥ia <mia.shopgal@gmail.com>
2024-01-31 18:32:04 +0800

    Use explicit JSON data for non 200 HTTP response tests

--- EssentialFeed/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -46,8 +46,8 @@ public final class RemoteFeedLoader {
             // but we pass .connectivity to completion
 
             switch result {
-                case let .success(data, _):
-                    if let root = try? JSONDecoder().decode(Root.self, from: data) {
+                case let .success(data, response):
+                    if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
                         completion(.success(root.items))
                     }
                     else {
