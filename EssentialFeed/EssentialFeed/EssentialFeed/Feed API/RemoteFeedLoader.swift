dc2492c62a142df5a94153932defbeeeab9fafea
₥ia <mia.shopgal@gmail.com>
2024-01-31 18:52:51 +0800

    Replace magic number with giving extract context

--- EssentialFeed/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -78,9 +78,11 @@ private class FeedItemMapper {
         }
     }
 
+    static var OK_200: Int { 200 }
+
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
 
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
 
