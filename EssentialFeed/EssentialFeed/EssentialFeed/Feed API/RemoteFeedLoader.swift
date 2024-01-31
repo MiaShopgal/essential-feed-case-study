1c802b0c49485efdb71fa27ce6802dcf87a3be34
₥ia <mia.shopgal@gmail.com>
2024-01-31 16:09:37 +0800

    Delivers invalidData error on non 200 HTTP response

--- EssentialFeed/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public protocol HTTPClient {
 
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 
 }
 
@@ -20,6 +20,7 @@ public final class RemoteFeedLoader {
 
     public enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
 
     public init(url: URL, client: HTTPClient) {
@@ -28,10 +29,15 @@ public final class RemoteFeedLoader {
     }
 
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error in
-            //note the error is "This is test", 
-            //but we pass .connectivity to completion
-            completion(.connectivity)
+        client.get(from: url) { error, response in
+            // note the error is "This is test",
+            // but we pass .connectivity to completion
+            if response != nil {
+                completion(.invalidData)
+            }
+            else {
+                completion(.connectivity)
+            }
         }
     }
 }
