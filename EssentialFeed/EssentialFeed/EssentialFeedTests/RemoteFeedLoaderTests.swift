bbc71b23ce526120d63511d56a1891d90060323c
₥ia <mia.shopgal@gmail.com>
2024-01-31 18:50:48 +0800

    Replace hardcode JSON with helper method

--- EssentialFeed/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -73,7 +73,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 
         expect(sut, toCompleteWith: .success([])) {
 
-            let emptyListJSON = Data(bytes: "{\"items\":[]}".utf8)
+            let emptyListJSON = makeItemsJSON([])
             client.complete(withStatusCode: 200, data: emptyListJSON)
         }
 
